10eb8105b395a08fda20fc1ea100dbbbd3d1a66f
Sotiris <38252842+Alchameth@users.noreply.github.com>
2022-07-17 11:47:26 +0300

    Update utils.ts

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -573,11 +573,11 @@ export function convertMSToDelayString(ms: number): string {
   return result;
 }
 
-export function successMessage(str, emoji = "<:zep_check:650361014180904971>") {
+export function successMessage(str, emoji = "<:zep_check:906897402101891093>") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
-export function errorMessage(str, emoji = "⚠") {
+export function errorMessage(str, emoji = "<:warn2:907942967329824778>") {
   return emoji ? `${emoji} ${str}` : str;
 }
 
